8e42c8ae166712d69ffef3a73f08a2c2549281b9
Brian Henry <brian.henry@delivered.io>
2017-10-28 12:41:21 -0500

    wrap js's in iffe
    to satisfy jslint.  add missing semicolons.

--- js/eventpage.js
+++ js/eventpage.js
@@ -1,123 +1,125 @@
-'use strict';
+(function() {
+  'use strict';
 
-var config = null;
+  var config = null;
 
-var getWindowData = function(callback) {
-  //note these are only for current profile...
-  chrome.windows.getAll({populate:true}, function(windows) {
-    var windowData = [];
-    windows.forEach(function(window) {
-      var tabData = [];
-      var windowDatum = {id: window.id, tabs: tabData};
-      
-      window.tabs.forEach(function(tab) {        
-        //output += tab.title + ": " + tab.url + "\n";
-        tabData.push({title: tab.title, url: tab.url});
+  var getWindowData = function(callback) {
+    //note these are only for current profile...
+    chrome.windows.getAll({populate:true}, function(windows) {
+      var windowData = [];
+      windows.forEach(function(window) {
+        var tabData = [];
+        var windowDatum = {id: window.id, tabs: tabData};
+        
+        window.tabs.forEach(function(tab) {        
+          //output += tab.title + ": " + tab.url + "\n";
+          tabData.push({title: tab.title, url: tab.url});
+        });
+
+        windowData.push(windowDatum);
+      });  
+      callback(windowData);
+    });
+  };
+
+  //returns whatever fxn returns, for convenience 
+  var withSnapshots = function(fxn) {
+    var snapshots = JSON.parse(localStorage.snapshots || null);
+    if(!snapshots) snapshots = [];
+    var returnval = fxn(snapshots);
+    localStorage.snapshots = JSON.stringify(snapshots);
+    return returnval;
+  };
+
+  //records data in localstorage for this extension
+  //note most recent first.
+  var recordSnapshot = function(windowData) {
+    withSnapshots(function (snapshots) {
+      snapshots.unshift({
+        date: new Date().toISOString(),
+        windows: windowData
       });
+    });
+  };
 
-      windowData.push(windowDatum);
+  var pruneSnapshots = function() {
+    withSnapshots(function (snapshots) {
+      while(snapshots.length >= config.numSnapshots) {
+        snapshots.pop();
+      }
+    });
+  };
+
+
+  var saveSessionInfo = function() {
+    console.log("saving @ " + new Date());
+    pruneSnapshots();
+    getWindowData(function(windowData) {
+      recordSnapshot(windowData); 
     });  
-    callback(windowData);
-  });
-};
+  };
 
-//returns whatever fxn returns, for convenience 
-var withSnapshots = function(fxn) {
-  var snapshots = JSON.parse(localStorage.snapshots || null);
-  if(!snapshots) snapshots = [];
-  var returnval = fxn(snapshots);
-  localStorage.snapshots = JSON.stringify(snapshots);
-  return returnval;
-};
 
-//records data in localstorage for this extension
-//note most recent first.
-var recordSnapshot = function(windowData) {
-  withSnapshots(function (snapshots) {
-    snapshots.unshift({
-      date: new Date().toISOString(),
-      windows: windowData
+  var getMostRecentSnapshot = function() {
+    return withSnapshots(function (snapshots) {
+      return snapshots.length > 0 ? snapshots[0] : null;
+    });
+  };
+
+
+  //callback will be invoked with global config object set up
+  var initConfig = function(cb) {
+    //can't really see sync storage in devtools, which sucks
+    chrome.storage.sync.get({
+      numSnapshots: 5, //default
+      saveIntervalMins: 45
+    }, function(items) {
+      //setting global config obj vals.
+      config = {
+        numSnapshots: items.numSnapshots,
+        saveIntervalMins: items.saveIntervalMins
+      };
+      
+      cb();
+    });  
+  };
+
+
+  var notifyConfigUpdate = function() {
+    console.log("updating config");
+    initConfig(function() {
+      //not worried about num snapshots.  that will work itself out eventually.
+      chrome.alarms.create('tabSave', {periodInMinutes: config.saveIntervalMins});
+    });
+  };
+
+
+  initConfig(function() {
+    chrome.runtime.onInstalled.addListener(function() {
+    
+  //not 100% sure this is getting called on initial install...  seem to have empty snapshots array in localStorage?  
+  //well, this did go fine when i installed on another profile.  not sure what the deal is.  some interaction with saving
+  //  options from options page too soon? 
+      saveSessionInfo();
+      chrome.alarms.create('tabSave', {periodInMinutes: config.saveIntervalMins});
+    });
+
+
+    //have to have this at top level (on in onInstalled, which only runs once).  
+    //  when event page loads again, the listener needs to be associated
+    chrome.alarms.onAlarm.addListener(function(alarm) {
+      console.log("alarm: " + alarm.name);
+      if(alarm.name == 'tabSave') saveSessionInfo();
     });
   });
-};
 
-var pruneSnapshots = function() {
-  withSnapshots(function (snapshots) {
-    while(snapshots.length >= config.numSnapshots) {
-      snapshots.pop();
-    }
+
+
+  chrome.browserAction.setBadgeText({text: "BG"});
+
+
+  chrome.runtime.onSuspend.addListener(function() {
+    console.log("Unloading.");
+    chrome.browserAction.setBadgeText({text: ""});
   });
-};
-
-
-var saveSessionInfo = function() {
-  console.log("saving @ " + new Date());
-  pruneSnapshots();
-  getWindowData(function(windowData) {
-    recordSnapshot(windowData); 
-  });  
-};
-
-
-var getMostRecentSnapshot = function() {
-  return withSnapshots(function (snapshots) {
-    return snapshots.length > 0 ? snapshots[0] : null;
-  });
-}
-
-
-//callback will be invoked with global config object set up
-var initConfig = function(cb) {
-  //can't really see sync storage in devtools, which sucks
-  chrome.storage.sync.get({
-    numSnapshots: 5, //default
-    saveIntervalMins: 45
-  }, function(items) {
-    //setting global config obj vals.
-    config = {
-      numSnapshots: items.numSnapshots,
-      saveIntervalMins: items.saveIntervalMins
-    };
-    
-    cb();
-  });  
-};
-
-
-var notifyConfigUpdate = function() {
-  console.log("updating config");
-  initConfig(function() {
-    //not worried about num snapshots.  that will work itself out eventually.
-    chrome.alarms.create('tabSave', {periodInMinutes: config.saveIntervalMins});
-  });
-};
-
-
-initConfig(function() {
-  chrome.runtime.onInstalled.addListener(function() {
-  
-//not 100% sure this is getting called on initial install...  seem to have empty snapshots array in localStorage?  
-//well, this did go fine when i installed on another profile.  not sure what the deal is.  some interaction with saving
-//  options from options page too soon? 
-    saveSessionInfo();
-    chrome.alarms.create('tabSave', {periodInMinutes: config.saveIntervalMins});
-  });
-
-
-  //have to have this at top level (on in onInstalled, which only runs once).  
-  //  when event page loads again, the listener needs to be associated
-  chrome.alarms.onAlarm.addListener(function(alarm) {
-    console.log("alarm: " + alarm.name);
-    if(alarm.name == 'tabSave') saveSessionInfo();
-  });
-});
-
-
-
-chrome.browserAction.setBadgeText({text: "BG"});
-
-
-chrome.runtime.onSuspend.addListener(function() {
-  console.log("Unloading.");
-  chrome.browserAction.setBadgeText({text: ""});
-});
+}());
--- js/popup.js
+++ js/popup.js
@@ -1,47 +1,49 @@
-'use strict';
+(function () {
+  'use strict';
 
-//redo in coffeescript?
-//promises?
+  //redo in coffeescript?
+  //promises?
 
-//bring in module system
-var $ = $,
-    moment = moment;
+  //bring in module system
+  var $ = $,
+      moment = moment;
 
-//try handlebars or mustache?  ember?
-var formatSnapshot = function(snapshot) {
-  var $root = $('<div>');
-  $root.addClass('root');
-  if(snapshot) {
-    //2015-05-30T00:04:17.316Z (ISO) converted to local
-    var snapMoment = moment(snapshot.date);
-    $root.text('Snapshot Date: ' + snapMoment.format('ddd M/D/YY h:mm:ss A') + " (" + snapMoment.fromNow() + ")");
-  
-    snapshot.windows.forEach(function(window, idx) {
-      var $windowElem = $('<div>');
-      $windowElem.addClass('window');
-      $root.append($windowElem);
-      $windowElem.text('Window:');
+  //try handlebars or mustache?  ember?
+  var formatSnapshot = function(snapshot) {
+    var $root = $('<div>');
+    $root.addClass('root');
+    if(snapshot) {
+      //2015-05-30T00:04:17.316Z (ISO) converted to local
+      var snapMoment = moment(snapshot.date);
+      $root.text('Snapshot Date: ' + snapMoment.format('ddd M/D/YY h:mm:ss A') + " (" + snapMoment.fromNow() + ")");
     
-      window.tabs.forEach(function(tab, idx) {
-        var $tabElem = $('<div>');
-        $tabElem.addClass('tab');
-        $windowElem.append($tabElem);
-        $tabElem.text(tab.url);    
+      snapshot.windows.forEach(function(window, idx) {
+        var $windowElem = $('<div>');
+        $windowElem.addClass('window');
+        $root.append($windowElem);
+        $windowElem.text('Window:');
+      
+        window.tabs.forEach(function(tab, idx) {
+          var $tabElem = $('<div>');
+          $tabElem.addClass('tab');
+          $windowElem.append($tabElem);
+          $tabElem.text(tab.url);    
+        });
       });
+    } else {
+      $root.text('No Snapshots Available');
+    }
+    return $root;
+  };
+
+
+  window.onload = function() {
+    //on extension button push, should show most recent session info saved for use in restoring  
+    
+    //could also show list of sessions and let user look at them and compare.
+    chrome.runtime.getBackgroundPage(function(window) { //note this is a DOM window object for the event page
+      var snappysnap = window.getMostRecentSnapshot();
+      $('#display').append(formatSnapshot(snappysnap));
     });
-  } else {
-    $root.text('No Snapshots Available');
-  }
-  return $root;
-};
-
-
-window.onload = function() {
-  //on extension button push, should show most recent session info saved for use in restoring  
-  
-  //could also show list of sessions and let user look at them and compare.
-  chrome.runtime.getBackgroundPage(function(window) { //note this is a DOM window object for the event page
-    var snappysnap = window.getMostRecentSnapshot();
-    $('#display').append(formatSnapshot(snappysnap));
-  });
-};
+  };
+}());
