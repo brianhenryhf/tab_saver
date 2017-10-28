c534ac721b6e02898a3d7cd1154f1c88675eb767
Brian Henry <brian.henry@delivered.io>
2017-10-28 14:25:28 -0500

    add tab launcher fxnality
    handles http, https, chrome-extension schemes. woo!

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,8 @@
+//whoah - can keep open the popup and debug with dev tools by opening chrome-extension://ikajjkncpkhnfnkdgjplapkldjjkblok/main.html 
+//  manually in a tab
 (function () {
   'use strict';
+
 
   //TODO redo in coffeescript?
   //TODO promises?
@@ -24,8 +27,9 @@
         $root.append($windowElem);
         $windowElem.text('Window:');
       
+        //$windowElem.append($('<ul>'));  more correct, but introduces some styling i don't want to mess with atm
         window.tabs.forEach(function(tab, idx) {
-          var $tabElem = $('<div>');
+          var $tabElem = $('<li>'); //BRH TODO these don't wrap.  fixed width for popup will truncate viewable.  s/ prolly fix.
           $tabElem.addClass('tab');
           $windowElem.append($tabElem);
           $tabElem.text(tab.url);    
@@ -38,9 +42,37 @@
   };
 
 
+  var extractUris = function(text) {
+    return text.split('\n').reduce(function(acc, curr) {
+      if(/^(http:\/\/|https:\/\/|chrome-extension:\/\/)\S+/.test(curr)) {
+        acc.push(curr);
+      } else {
+        console.info('unacceptable uri skipped: ' + curr);
+      }
+      return acc;
+    }, []);
+  };
+
+  var lauchTabs = function(uriAry) {
+    uriAry.forEach(function(uri){
+      console.info('launching tab: ' + uri);
+      chrome.tabs.create({url:uri});
+    });
+  };
+
   window.onload = function() {
     //on extension button push, should show most recent session info saved for use in restoring  
     console.debug('popup onload'); //note this is off by default in chrome console
+
+    //BRH NOTE could use some more general component construct and data- tags to determine what's submitted.  for now, this is fine.
+    $('#tab-launcher > .js-submit').on('click', function(evt){
+      console.debug('submit clicked');
+      var $tabLauncher = $('#tab-launcher'),
+          $textInput = $tabLauncher.find('.js-text');
+
+      var uris = extractUris($textInput.val().trim());
+      if (uris) lauchTabs(uris);
+    });
 
     //could also show list of sessions and let user look at them and compare?
     chrome.runtime.getBackgroundPage(function(window) { //note this is a DOM window object for the event page
